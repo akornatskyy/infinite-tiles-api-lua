474795a17271149c2325f83f212b913e583957be
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-04-09 20:48:41 +0300

    Added validation rule for place protocol command.

--- src/validators.lua
+++ src/validators.lua
@@ -5,11 +5,19 @@ local range = require 'validation.rules.range'
 local rules = require 'rules'
 
 local allowed_keys = rules.allowed_keys
+local number = rules.number
+
+local coordinate_rule = {number, range{-100, 100}}
 
 return {
   tiles = validator.new {
     __ERROR__ = {allowed_keys {'t', 'area', 'ref', 'coords'}},
     area = {required},
     ref = {range {min = 0, max = 127}}
+  },
+  place = validator.new {
+    __ERROR__ = {allowed_keys {'t', 'x', 'y'}},
+    x = coordinate_rule,
+    y = coordinate_rule
   }
 }

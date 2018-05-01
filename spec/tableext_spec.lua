15e412c1e6b5f1724e5d65744d3c02808ef3cd56
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-05-01 20:15:49 +0300

    Added tableext sub which subtracts keys in two tables.

--- spec/tableext_spec.lua
+++ spec/tableext_spec.lua
@@ -3,6 +3,22 @@ local tableext = require 'tableext'
 local describe, it, assert = describe, it, assert
 
 describe('tableext', function()
+  describe('sub', function()
+    it('returns keys from t1 not present in t2', function()
+      local cases = {
+        {{}, {}, {}},
+        {{'a'}, {a = 1}, {}},
+        {{}, {}, {a = 1}},
+        {{'a'}, {a = 1, b = 1}, {b = 1, c = 1}}
+      }
+      for _, args in next, cases do
+        local expected, t1, t2 = unpack(args)
+        local r = tableext.sub(t1, t2)
+        assert.same(expected, r)
+      end
+    end)
+  end)
+
   describe('prefix', function()
     it('returns a table with all elements prefixed', function()
       local r = tableext.prefix({'k1', 'k2'}, 'prefix:')
--- src/tableext.lua
+++ src/tableext.lua
@@ -1,3 +1,16 @@
+-- Returns keys from t1 which are not in t2.
+local function table_sub(t1, t2)
+  local r = {}
+  local i = 1
+  for key in next, t1 do
+    if not t2[key] then
+      r[i] = key
+      i = i + 1
+    end
+  end
+  return r
+end
+
 local function table_prefix(t, prefix)
   local r = {}
   for i = 1, #t do
@@ -7,5 +20,6 @@ local function table_prefix(t, prefix)
 end
 
 return {
+  sub = table_sub,
   prefix = table_prefix
 }

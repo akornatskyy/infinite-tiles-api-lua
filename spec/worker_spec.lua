f721ce191be8aeb99bf1e68c2f0254c981eebc35
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-05-05 20:34:05 +0300

    Added worker test case when lock cannot be acquired.

---
+++ spec/worker_spec.lua
@@ -0,0 +1,38 @@
+local describe, it, assert = describe, it, assert
+
+local function load(script)
+  local f = 'return function(ARGV)\n' .. script .. '\nend'
+  return assert(loadstring(f))()
+end
+
+local function setup_redis(expected)
+  local i = 1
+  _G.redis.call = function(...)
+    local args, result = unpack(expected[i])
+    assert.same(args, {...})
+    i = i + 1
+    return result
+  end
+end
+
+describe('worker', function()
+  insulate('lifetime script', function()
+    package.loaded.redis = {}
+    package.loaded.socket = {}
+    local worker = require 'worker'
+    _G.redis = {}
+    _G.cmsgpack = {
+      unpack = function(d) return d end,
+      pack = function(d) return d end
+    }
+    local f = load(worker.sources.LIFETIME)
+
+    it('returns if lock cannot be acquired', function()
+      setup_redis {
+        {{'set', 'LOCK:LIFETIME:123', '', 'EX', '1', 'NX'}, false}
+      }
+
+      assert.is_false(f {'123'})
+    end)
+  end)
+end)

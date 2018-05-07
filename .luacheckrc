1b7c53eea9286b05fe143d9007c79db833d5d1ad
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-05-07 20:38:20 +0300

    Added worker test case when lifetime script removes expired object.

--- .luacheckrc
+++ .luacheckrc
@@ -3,7 +3,7 @@ cache = true
 codes = true
 globals = {'_ENV', 'ngx'}
 files['spec/'].read_globals = {
-  'before_each', 'describe', 'insulate', 'it', 'after_each'
+  'before_each', 'describe', 'insulate', 'it', 'after_each', 'mock'
 }
 unused_args = false
 ignore = {}
--- spec/worker_spec.lua
+++ spec/worker_spec.lua
@@ -1,4 +1,4 @@
-local describe, it, assert = describe, it, assert
+local describe, it, assert, mock = describe, it, assert, mock
 
 local function load(script)
   local f = 'return function(ARGV)\n' .. script .. '\nend'
@@ -44,6 +44,27 @@ describe('worker', function()
       }
 
       assert.is_true(f {'123'})
+    end)
+
+    it('removes object', function()
+      local mock_msgpack = mock(_G.cmsgpack)
+      local object = {area = 'N0W0', cell = 5}
+      local packet = {t = 'remove', objects = {'123'}}
+      setup_redis {
+        {{'set', 'LOCK:LIFETIME:123', '', 'EX', '1', 'NX'}, true},
+        {{'get', 'OBJECT:123'}, object},
+        {{'lrem', 'A:OBJECTS:N0W0', '1', '123'}},
+        {{'setbit', 'A:CELLS:N0W0', 5, 0}},
+        {{'del', 'OBJECT:123'}},
+        {{'publish', 'AREA:N0W0', packet}},
+        {{'zrem', 'LIFETIME', '123'}},
+        {{'del', 'LOCK:LIFETIME:123'}}
+      }
+
+      assert.is_true(f {'123'})
+
+      assert.spy(mock_msgpack.unpack).was.called_with(object)
+      assert.spy(mock_msgpack.pack).was.called_with(packet)
     end)
   end)
 end)

7552f49c60c665b9a7cb40800adff2eb4396edc2
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-06-03 20:03:41 +0300

    Added list table subtraction.

--- src/tableext.lua
+++ src/tableext.lua
@@ -1,16 +1,3 @@
--- Returns keys from t1 which are not in t2.
-local function table_sub(t1, t2)
-  local r = {}
-  local i = 1
-  for key in next, t1 do
-    if not t2[key] then
-      r[i] = key
-      i = i + 1
-    end
-  end
-  return r
-end
-
 local function table_prefix(t, prefix)
   local r = {}
   for i = 1, #t do
@@ -36,8 +23,23 @@ local function table_add_unique(t1, t2)
   end
 end
 
+-- Removes elements from t1 which are in t2.
+local function table_sub(t1, t2)
+  local n = #t2
+  for i = #t1, 1, -1 do
+    local value = t1[i]
+    for j = 1, n do
+      if value == t2[j] then
+        table.remove(t1, i)
+        break
+      end
+    end
+  end
+  return t1
+end
+
 return {
-  sub = table_sub,
   prefix = table_prefix,
-  add_unique = table_add_unique
+  add_unique = table_add_unique,
+  sub = table_sub
 }

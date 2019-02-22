aef4ca85a4221e2831f269c611c1c63e7ed4cee9
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-02-22 20:52:15 +0200

    Cleanup in validation rules.

--- src/rules.lua
+++ src/rules.lua
@@ -1,111 +1,36 @@
-local next = next
+local unpack = unpack
 
-local AllowedFields = {
-  __index = {
-    validate = function(self, value, model)
-      for name in next, model do
-        if not self.allowed[name] then
-          return 'Unknown field name [' .. name .. '].'
-        end
-      end
-      return nil
-    end
-  }
-}
-
-local function allowed_fields(t)
-  local allowed = {}
-  for i = 1, #t do
-    allowed[t[i]] = true
+local function area(value)
+  if #value ~= 4 then
+    return 'The area list must have exactly four integers.'
   end
-  return setmetatable({allowed = allowed}, AllowedFields)
+  local xmin, ymin, dx, dy = unpack(value)
+  if xmin < -100 or xmin > 100 then
+    return 'The xmin must be [-100, 100].'
+  end
+  if ymin < -100 or ymin > 100 then
+    return 'The ymin must be [-100, 100].'
+  end
+  if dx < 0 or dx > 12 then
+    return 'The dx must be [0, 12].'
+  end
+  if dy < 0 or dy > 23 then
+    return 'The dy must be [0, 23].'
+  end
+  return nil
 end
 
-local number = {
-  validate = function(self, value, model)
-    if not value then
-      return 'Required field cannot be left blank.'
-    end
-    if type(value) ~= 'number' then
-      return 'Must be a number value.'
-    end
-    return nil
+local function coords(value)
+  if #value % 2 ~= 0 then
+    return 'Must include pairs.'
   end
-}
-
-local integer = {
-  validate = function(self, value, model)
-    if not value then
-      return 'Required field cannot be left blank.'
-    end
-    if type(value) ~= 'number' or value % 1 ~= 0 then
-      return 'Must be an integer value.'
-    end
-    return nil
+  -- 12 * 23 * 2 = 552
+  if #value > 624 then
+    return 'Exceeds maximum length of 624.'
   end
-}
-
-local area = {
-  validate = function(self, value, model)
-    if not value or type(value) ~= 'table' then
-      return 'Must be a list value.'
-    end
-    if #value ~= 4 then
-      return 'The list must have exactly four integers.'
-    end
-    for _, n in next, value do
-      if type(n) ~= 'number' or n % 1 ~= 0 then
-        return 'The list must have integers only.'
-      end
-    end
-    local xmin, ymin, dx, dy = unpack(value)
-    if xmin < -100 or xmin > 100 then
-      return 'The xmin must be [-100, 100].'
-    end
-    if ymin < -100 or ymin > 100 then
-      return 'The ymin must be [-100, 100].'
-    end
-    if dx < 0 or dx > 12 then
-      return 'The dx must be [0, 12].'
-    end
-    if dy < 0 or dy > 23 then
-      return 'The dy must be [0, 23].'
-    end
-    return nil
-  end
-}
-
-local coords = {
-  validate = function(self, value, model)
-    if not value then
-      return nil
-    end
-    if type(value) ~= 'table' then
-      return 'Must be a list value.'
-    end
-    if #value % 2 ~= 0 then
-      return 'Must include pairs.'
-    end
-    -- 12 * 23 * 2 = 552
-    if #value > 624 then
-      return 'Exceeds maximum length of 624.'
-    end
-    for _, n in next, value do
-      if type(n) ~= 'number' or n % 1 ~= 0 then
-        return 'The list must have integers only.'
-      end
-      if n < 0 or n > 23 then
-        return 'The coords must be relative.'
-      end
-    end
-    return nil
-  end
-}
+end
 
 return {
-  allowed_fields = allowed_fields,
   area = area,
-  coords = coords,
-  integer = integer,
-  number = number
+  coords = coords
 }
--- src/validators.lua
+++ src/validators.lua
@@ -1,38 +1,48 @@
-local validator = require 'validation.validator'
+local fields = require 'validation.rules.fields'
+local items = require 'validation.rules.items'
 local length = require 'validation.rules.length'
 local range = require 'validation.rules.range'
 local required = require 'validation.rules.required'
+local rule = require 'validation.rules.rule'
+local typeof = require 'validation.rules.typeof'
+local optional = require 'validation.rules.optional'
+local validator = require 'validation.validator'
 
 local rules = require 'rules'
 
-local allowed_fields = rules.allowed_fields
-local area = rules.area
-local coords = rules.coords
-local integer = rules.integer
-local number = rules.number
-
-local coordinate_rule = {integer, range {-100, 100}}
+local coordinate_rules = {required, typeof 'integer', range {-100, 100}}
 
 return {
   ping = validator.new {
-    __ERROR__ = {allowed_fields {'t', 'time'}},
-    time = {number, range {min = 1530902228}}
+    __ERROR__ = {fields {'t', 'time'}},
+    time = {required, typeof 'number', range {min = 1530902228}}
   },
   tiles = validator.new {
-    __ERROR__ = {allowed_fields {'t', 'area', 'ref', 'coords'}},
-    area = {required, area},
-    ref = {range {min = 0, max = 127}},
-    coords = {coords}
+    __ERROR__ = {fields {'t', 'area', 'ref', 'coords'}},
+    area = {
+      required,
+      typeof 'table',
+      items {typeof 'integer'},
+      rule(rules.area)
+    },
+    ref = {optional {typeof 'integer', range {min = 0, max = 127}}},
+    coords = {
+      optional {
+        typeof 'table',
+        rule(rules.coords),
+        items {typeof 'integer', range {min = 0, max = 23}}
+      }
+    }
   },
   place = validator.new {
-    __ERROR__ = {allowed_fields {'t', 'x', 'y'}},
-    x = coordinate_rule,
-    y = coordinate_rule
+    __ERROR__ = {fields {'t', 'x', 'y'}},
+    x = coordinate_rules,
+    y = coordinate_rules
   },
   move = validator.new {
-    __ERROR__ = {allowed_fields {'t', 'id', 'x', 'y'}},
-    id = {required, length {min = 8, max = 8}},
-    x = coordinate_rule,
-    y = coordinate_rule
+    __ERROR__ = {fields {'t', 'id', 'x', 'y'}},
+    id = {required, typeof 'string', length {min = 8, max = 8}},
+    x = coordinate_rules,
+    y = coordinate_rules
   }
 }
